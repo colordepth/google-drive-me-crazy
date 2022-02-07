4f59cc6a56401f33663c4eabf4915ca3aacdf1e0
Deep Sharma <deepsharma.cs@gmail.com>
2022-02-08 00:24:45 +0530

    bugfix accessing path before initialization

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
+const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../../.env')});
 const express = require('express');
-const path = require('path');
 const {google} = require('googleapis');
 const axios = require('axios');
 const app = express();
