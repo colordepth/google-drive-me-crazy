e1c708867548197402410f31b507dd24aba91152
Deep Sharma <deepsharma.cs@gmail.com>
2022-04-04 03:35:23 +0530

    Add basic dashboard

--- src/client/src/components/Dashboard.jsx
+++ src/client/src/components/Dashboard.jsx
@@ -1,24 +1,66 @@
-import { Link } from "react-router-dom";
+import { Icon } from "@blueprintjs/core";
+import { useDispatch, useSelector } from "react-redux";
+import { openPath } from "../services/tabSlice";
+import { selectUsers } from "../services/userSlice";
 
-const Dashboard = () => {
+import './Dashboard.css';
+
+const Dashboard = ({tab}) => {
+  const users = useSelector(selectUsers);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <div className='DashboardElement'>
-        <div className='DashboardTitle'>Pinned folders</div>
+        <div className='DashboardTitle'>
+          <Icon icon='unpin'/> <h2>Pinned</h2>
+        </div>
         <div className='DashboardElementContents'>
-          1 2 3
+          <div className='UserCard'>
+            sample
+          </div>
         </div>
       </div>
       <div className='DashboardElement'>
-        <div className='DashboardTitle'>User drives</div>
+        <div className='DashboardTitle' style={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+          <Icon icon='database' /> <h2>User Drives</h2>
+        </div>
+        <div className='DashboardElementContents'>
+          {
+            users.map(user => {
+              return <>
+                <div key={user.id} className='UserCard'>
+                  <div>
+                    <img src={user.photoLink} style={{borderRadius: '50%'}}/>
+                  </div>
+                  <div>
+                    <div onClick={() => {
+                        dispatch(openPath({
+                          id: tab.id, 
+                          path: {
+                            path: 'root',
+                            name: "College",
+                            userID: user.minifiedID
+                          }
+                        }));
+                      }}
+                    >
+                      <b style={{color: '#333'}}>College</b><br/><br/>
+                      <span style={{color: '#555'}}>{user.emailAddress}</span><br/>
+                    </div>
+                  </div>
+                </div>
+              </>
+            })
+          }
+        </div>
+      </div>
+      <div className='DashboardElement'>
+        <div className='DashboardTitle' style={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+          <Icon icon='time' /> <h2>Recent Files</h2>
+        </div>
         <div className='DashboardElementContents'>
           1 2 3
-          <div>
-            <Link to='/qvuQXkR7SAA=/storage-analyzer'>Storage</Link>
-          </div>
-          <div>
-            <Link to='/qvuQXkR7SAA=/root'>File Explorer</Link>
-          </div>
         </div>
       </div>
     </div>
