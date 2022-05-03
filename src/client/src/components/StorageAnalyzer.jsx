66552d64c2979fd832fe14d4aa32111dee09bdbe
Deep Sharma <deepsharma.cs@gmail.com>
2022-05-03 12:02:12 +0530

    Rollback to old Storage Analyzer

--- src/client/src/components/StorageAnalyzer.jsx
+++ src/client/src/components/StorageAnalyzer.jsx
@@ -1,352 +1,34 @@
-import { useEffect, memo } from 'react';
+import { useEffect, useState, useRef, memo } from 'react';
 import ReactECharts from 'echarts-for-react';
 import { useSelector } from 'react-redux';
 
-import NavigationBar from './NavigationBar';
+import FileElementList from './FileElementList';
+import StatusBar from './StatusBar';
 import './StorageAnalyzer.css';
 
-import { selectFilesForUser } from '../services/fileManagerService';
+import { selectFilesForUser, selectActiveMajorFetchCount } from '../services/fileManagerService';
 import { selectUserByID } from '../services/userSlice';
 
 import { humanFileSize } from '../services/filesMiscellaneous';
+import ToolBar from './ToolBar';
 
-const item1 = {
-  color: '#F54F4A'
-};
-const item2 = {
-  color: '#FF8C75'
-};
-const item3 = {
-  color: '#FFB499'
-};
-const sunburstData = [
-  {
-    children: [
-      {
-        value: 5,
-        children: [
-          {
-            value: 1,
-            itemStyle: item1
-          },
-          {
-            value: 2,
-            children: [
-              {
-                value: 1,
-                itemStyle: item2
-              }
-            ]
-          },
-          {
-            children: [
-              {
-                value: 1
-              }
-            ]
-          }
-        ],
-        itemStyle: item1
-      },
-      {
-        value: 10,
-        children: [
-          {
-            value: 6,
-            children: [
-              {
-                value: 1,
-                itemStyle: item1
-              },
-              {
-                value: 1
-              },
-              {
-                value: 1,
-                itemStyle: item2
-              },
-              {
-                value: 1
-              }
-            ],
-            itemStyle: item3
-          },
-          {
-            value: 2,
-            children: [
-              {
-                value: 1
-              }
-            ],
-            itemStyle: item3
-          },
-          {
-            children: [
-              {
-                value: 1,
-                itemStyle: item2
-              }
-            ]
-          }
-        ],
-        itemStyle: item1
-      }
-    ],
-    itemStyle: item1
-  },
-  {
-    value: 9,
-    children: [
-      {
-        value: 4,
-        children: [
-          {
-            value: 2,
-            itemStyle: item2
-          },
-          {
-            children: [
-              {
-                value: 1,
-                itemStyle: item1
-              }
-            ]
-          }
-        ],
-        itemStyle: item1
-      },
-      {
-        children: [
-          {
-            value: 3,
-            children: [
-              {
-                value: 1
-              },
-              {
-                value: 1,
-                itemStyle: item2
-              }
-            ]
-          }
-        ],
-        itemStyle: item3
-      }
-    ],
-    itemStyle: item2
-  },
-  {
-    value: 7,
-    children: [
-      {
-        children: [
-          {
-            value: 1,
-            itemStyle: item3
-          },
-          {
-            value: 3,
-            children: [
-              {
-                value: 1,
-                itemStyle: item2
-              },
-              {
-                value: 1
-              }
-            ],
-            itemStyle: item2
-          },
-          {
-            value: 2,
-            children: [
-              {
-                value: 1
-              },
-              {
-                value: 1,
-                itemStyle: item1
-              }
-            ],
-            itemStyle: item1
-          }
-        ],
-        itemStyle: item3
-      }
-    ],
-    itemStyle: item1
-  },
-  {
-    children: [
-      {
-        value: 6,
-        children: [
-          {
-            value: 1,
-            itemStyle: item2
-          },
-          {
-            value: 2,
-            children: [
-              {
-                value: 2,
-                itemStyle: item2
-              }
-            ],
-            itemStyle: item1
-          },
-          {
-            value: 1,
-            itemStyle: item3
-          }
-        ],
-        itemStyle: item3
-      },
-      {
-        value: 3,
-        children: [
-          {
-            value: 1
-          },
-          {
-            children: [
-              {
-                value: 1,
-                itemStyle: item2
-              }
-            ]
-          },
-          {
-            value: 1
-          }
-        ],
-        itemStyle: item3
-      }
-    ],
-    itemStyle: item1
-  }
-];
-
-const sunburstOption = {
-  series: {
-    radius: ['15%', '80%'],
-    type: 'sunburst',
-    sort: undefined,
-    emphasis: {
-      focus: 'ancestor'
-    },
-    data: sunburstData,
-    label: {
-      rotate: 'radial'
-    },
-    levels: [],
-    itemStyle: {
-      color: '#ddd',
-      borderWidth: 2
-    }
-  }
-};
-
-const driveActivityOptions = {
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-      label: {
-        backgroundColor: '#6a7985'
-      }
-    }
-  },
-  legend: {
-    data: ['Documents', 'Videos', 'Images', 'Compressed', 'Other']
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis: [
-    {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value'
-    }
-  ],
-  series: [
-    {
-      name: 'Documents',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      name: 'Videos',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [220, 182, 191, 234, 290, 330, 310]
-    },
-    {
-      name: 'Images',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [150, 232, 201, 154, 190, 330, 410]
-    },
-    {
-      name: 'Compressed',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [320, 332, 301, 334, 390, 330, 320]
-    },
-    {
-      name: 'Other',
-      type: 'line',
-      stack: 'Total',
-      label: {
-        show: true,
-        position: 'top'
-      },
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [820, 932, 901, 934, 1290, 1330, 1320]
-    }
-  ]
-};
-
-const halfPieOptions = {
+const defaultOptions = {
   responsive: true,
   maintainAspectRatio: false,
   animationDurationUpdate: 5000,
+  legend: {
+    orient: 'horizontal',
+    x: 'center',
+    y: '12%',
+    data: []
+  },
+  title: {
+    text: '',
+    left: 'center',
+    top: 'top'
+  },
   tooltip: {
-    trigger: "item",
+    trigger: "item",  //item
     axisPointer: {
       type: "shadow",
     },
@@ -366,19 +48,50 @@ const halfPieOptions = {
   },
   series: [
     {
-      startAngle: 180,
-      endAngle: 360,
       name: '',
       type: 'pie',
-      label: {
-        show: false,
-      },
       itemStyle: {
         borderRadius: 0,
         borderColor: '#f8f6fb',
         borderWidth: 0
       },
+      // labelLine: {
+      //   length: 30
+      // },
+      // label: {
+      //   formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+      //   backgroundColor: '#F6F8FC',
+      //   borderColor: '#8C8D8E',
+      //   borderWidth: 1,
+      //   borderRadius: 4,
+      //   rich: {
+      //     a: {
+      //       color: '#6E7079',
+      //       lineHeight: 22,
+      //       align: 'center'
+      //     },
+      //     hr: {
+      //       borderColor: '#8C8D8E',
+      //       width: '100%',
+      //       borderWidth: 1,
+      //       height: 0
+      //     },
+      //     b: {
+      //       color: '#4C5058',
+      //       fontSize: 14,
+      //       fontWeight: 'bold',
+      //       lineHeight: 33
+      //     },
+      //     per: {
+      //       color: '#fff',
+      //       backgroundColor: '#4C5058',
+      //       padding: [3, 4],
+      //       borderRadius: 4
+      //     }
+      //   }
+      // },
       data: [],
+      // radius: ['35%', '60%'],
       radius: ['30%', '50%'],
       center: ['50%', '67%']
     }
@@ -395,97 +108,44 @@ function updateChart(chartName, data, dataPoint, title='') {
 
   const chartRef = chartRefs[chartName].getEchartsInstance();
 
-  const invisibleHalf = data.reduce((halfObj, curObj) => {
-    halfObj.quotaBytesUsed += curObj.quotaBytesUsed;
-    halfObj.count += curObj.count;
-    return halfObj
-  }, {
-    name: null,
-    count: 0,
-    quotaBytesUsed: 0,
-    itemStyle:{opacity:0},
-    tooltip:{show:false} 
-  });
+  const label = {
+    formatter: (params) => {
 
-  const newSeries = { ...halfPieOptions.series, title, data: data.concat(invisibleHalf).map(obj => {return {...obj, value: obj[dataPoint]}}) };
-  const newOption = { ...halfPieOptions, series: newSeries };
+      let accompanyingValue = params.data[dataPoint];
+
+      if (dataPoint == 'quotaBytesUsed') {
+        accompanyingValue = humanFileSize(accompanyingValue);
+      }
+
+      return `${params.data.name} (${accompanyingValue})`
+    }
+  }
+
+  const newSeries = { ...defaultOptions.series, title, label, data: data.map(obj => {return {...obj, value: obj[dataPoint]}}) };
+  const newTitle =  { ...defaultOptions.title, text: title };
+  const newLegend = { ...defaultOptions.legend, data: data.map(obj => obj.name) }
+  const newOption = { ...defaultOptions, title: newTitle, legend: newLegend, series: newSeries };
 
   // chartRef.resize();
   chartRef.setOption(newOption);
-
-  chartRef.on('mousemove', params => {
-    if (params.data.name === null) chartRef.getZr().setCursorStyle('default')
-  })
 }
 
-const DonutChart = memo(({name, className}) => {
+const DonutChart = memo(({name}) => {
+  const chartRef = useRef(null);
 
   return (
-    <div className={className}>
-      <div className='HalfDonutContainer'>
-        <ReactECharts
-          option={halfPieOptions}
-          notMerge={false}
-          lazyUpdate={false}
-          ref={(e) => { chartRefs[name] = e }}
-          onChartReady={() => console.log("chart", name, "ready")}
-          onEvents={{'click': (event) => console.log("clicked", event)}}
-          className='FileTypeChart'
-          // width={100}
-          // height={100}
-          // opts={{ renderer: "svg" }}
-        />
-      </div>
-      <div className='HalfDonutLegend'>
-        <li>Application</li>
-        <li>Documents</li>
-        <li>Videos</li>
-        <li>Documents</li>
-        <li>Other</li>
-      </div>
-    </div>
-  );
-});
-
-const DriveActivityChart = memo(({name, className}) => {
-
-  return (
-    <div className={className} style={{height: '300px'}}>
-      <h1>Drive Activity</h1>
-      <ReactECharts
-        option={driveActivityOptions}
-        notMerge={false}
-        lazyUpdate={false}
-        ref={(e) => { chartRefs[name] = e }}
-        onChartReady={() => console.log("chart", name, "ready")}
-        onEvents={{'click': (event) => console.log("clicked", event)}}
-        className='DriveActivityChart'
-        // width='100%'
-        height={100}
-        // opts={{ renderer: "svg" }}
-      />
-    </div>
-  );
-});
-
-const FoldersSunburstChart = memo(({name, className}) => {
-
-  return (
-    <div className={className} style={{height: '350px'}}>
-      <h2>Folder Size</h2>
-      <ReactECharts
-        option={sunburstOption}
-        notMerge={false}
-        lazyUpdate={false}
-        ref={(e) => { chartRefs[name] = e }}
-        onChartReady={() => console.log("chart", name, "ready")}
-        onEvents={{'click': (event) => console.log("clicked", event)}}
-        className='FolderSunburstChart'
-        // width='100%'
-        // height={100}
-        // opts={{ renderer: "svg" }}
-      />
-    </div>
+    <ReactECharts
+      option={defaultOptions}
+      notMerge={false}
+      lazyUpdate={false}
+      ref={(e) => { chartRefs[name] = e }}
+      onChartReady={() => console.log("chart ready")}
+      onEvents={{'click': (event) => console.log("clicked", event)}}
+      className='FileTypeChart'
+      // width={100}
+      // height={100}
+      // opts={{ renderer: "svg" }}
+    />
   );
 });
 
@@ -496,6 +156,7 @@ const StorageAnalyzer = ({ userID, tab }) => {
   const files = allFiles && allFiles.filter(file => 
     file.owners && file.owners.length && file.owners[0].me
   );
+  // const activeMajorFetchCount = useSelector(selectActiveMajorFetchCount(userID));
 
   console.log("Storage analyzer rerender");
 
@@ -536,24 +197,38 @@ const StorageAnalyzer = ({ userID, tab }) => {
     });
 
     updateChart('fileSize', newData, 'quotaBytesUsed', 'File size');
+    updateChart('fileCount', newData, 'count', 'Number of files');
     
   }, [files]);
 
+  const highlightedEntitiesList = Object
+    .keys(tab.highlightedEntities)
+    .map(entityID => tab.highlightedEntities[entityID]);
 
   return (
     <div className='StorageAnalyzer'>
-      <NavigationBar tab={ tab } user= { user } />
+      {/* <ToolBar
+        highlightedEntitiesList={ highlightedEntitiesList }
+        user={ user }
+        targetFolderID={ 'storage-analyzer' }
+        viewMode={ 'detail-view' }
+        setViewMode={ () => {} }
+      /> */}
       <div className='StorageGraphs'>
-        <div className='TopLeft Card'>
-          Network stuff
-        </div>
-        <div className='BottomLeft Card'>
-          extensions
-        </div>
-        <DriveActivityChart name='driveActivity' className='Middle Card' />
-        <FoldersSunburstChart name='folderSunburst' className='BottomRight Card' />
-        <DonutChart name='fileSize' className='TopRight Card'/>
+        <DonutChart name='fileSize'/>
+        <DonutChart name='fileCount'/>
       </div>
+      <FileElementList
+        loading={!(files && files.length)}
+        entities={files}
+        foldersFirst={false}
+        sortBy='quotaBytesUsed'
+        limit={100}
+        user={user}
+        tabID={tab.id}
+        view='detail-view'
+      />
+      <StatusBar noOfFiles={files && files.length}/>
     </div>
   );  
 }
